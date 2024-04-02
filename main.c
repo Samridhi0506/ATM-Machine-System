4106169a9d44e6929afe33890f38d23dd5dff144
Samridhi <samridhi470@gmail.com>
2024-04-02 17:45:08 +0530

    added minimum balance feature

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 // Initializing the balance amount as 0.
 float balance_amount = 0.0;
+float min_balance = 500.0;
+
 
 // Function for operations in ATM Machine
 void atm_machine_transaction();
@@ -39,6 +41,11 @@ void atm_machine_transaction()
         scanf("%f", &deposit_amount);
         balance_amount += deposit_amount;
         printf("Your current balance is Rs. %.2f. Thanks for depositing!\n", balance_amount);
+        // checking for minimum balance
+        if (balance_amount > min_balance)
+        {
+            printf("Your balance has crossed the minimum balance limit.\n");
+        }
         printf("Do you want a new transaction?\nPress 1 to 'proceed' and 2 to 'exit' from here\n");
         printf("Your option: ");
         scanf("%d", &new_transaction);
@@ -50,6 +57,20 @@ void atm_machine_transaction()
 
     // Case for Money Withdrawal
     case 2:
+        // checking for mininum balance
+        if (balance_amount < min_balance)
+        {
+            printf("Your balance is below the minimum balance limit. Please deposit money to proceed.\n");
+            printf("Do you want a new transaction?\nPress 1 to 'proceed' and 2 to 'exit' from here\n");
+            printf("Your option: ");
+            scanf("%d", &new_transaction);
+            if (new_transaction == 1)
+            {
+                atm_machine_transaction();
+            }
+        }
+        else
+        {
         printf("\nPlease enter amount to withdraw: ");
         scanf("%f", &withdraw_amount);
         if (withdraw_amount > balance_amount)
@@ -75,11 +96,18 @@ void atm_machine_transaction()
                 atm_machine_transaction();
             }
         }
+        }
         break;
+
 
     // Case for checking current bank amount
     case 3:
         printf("Your current bank balance is: %.2f\n", balance_amount);
+        // checking of minimum balance
+        if (balance_amount < min_balance)
+        {
+            printf("Your balance is below the minimum balance limit.\n");
+        }
         printf("Do you want a new transaction?\nPress 1 to 'proceed' and 2 to 'exit' from here\n");
         printf("Your option: ");
         scanf("%d", &new_transaction);
@@ -101,4 +129,3 @@ void atm_machine_transaction()
         }
     }
 }
-
